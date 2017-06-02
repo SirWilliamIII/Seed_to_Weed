ba344b19f6fbc56dd119d52da8ef2ce65cc78285
Chase Hemerda <chasehemerda@icloud.com>
2017-06-02 14:44:28 -0600

    change fonts

--- client/script.js
+++ client/script.js
@@ -7,9 +7,7 @@ $(document).ready(function(){
         }
         $("#strain-names").change(function(){
             var val = $("#strain-names").val();
-            console.log(val)
             if(val == "Blue Dream"){
-                console.log(data[0].flowering_time)
                 $("#flower-time").html(`<h2>FLOWERING TIME:</h2><h4>${data[0].flowering_time}</h4>`)
                 $("#yield").html(`<h2>YIELD:<h4>${data[0].yield}</h4>`)
                 $("#grow-difficulty").html(`<h2>GROW DIFFICULTY:<h4>${data[0].difficulty}</h4>`)
